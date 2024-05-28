1152b622e0a23159e05baf4d73a1278492f30a97
Atulshukla25 <141546324+Atulshukla25@users.noreply.github.com>
2024-05-28 11:37:22 +0000

    conversion

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -1,14 +1,18 @@
 let score="33abc"
-
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 let isLoggedIn=0
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
