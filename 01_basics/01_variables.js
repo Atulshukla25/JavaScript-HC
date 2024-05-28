28eaa7eba7269060c246cb9626080c4e1e18164e
Atulshukla25 <141546324+Atulshukla25@users.noreply.github.com>
2024-05-28 06:06:34 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=2129007
+let accountEmail="atul123@gmail.com"
+var accountpasswords="123456"
+accountCity="Ludhiana"
+let accountState;
+
+// accountId=2
+accountEmail="lucky123@gmail.com"
+accountpasswords="252525"
+accountCity="Ayodhya"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpasswords, accountCity, accountState])
